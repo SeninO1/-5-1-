a4dfc4df6d9eb293ab4be18c32f9120dab73191f
SeninO1 <151605111+SeninO1@users.noreply.github.com>
2023-11-27 00:22:38 +0300

    Add files via upload

---
+++ (урок 5) 1 задание.py
@@ -0,0 +1,13 @@
+x = float(input())
+if x == 0:
+print('нулевое')
+else:
+if x>0:
+print('положительное')
+elif x<0:
+print('отрицательное')
+if x % 2 == 0:
+print('четное')
+elif x % 2 != 0:
+print('нечетное')
+print('число')
